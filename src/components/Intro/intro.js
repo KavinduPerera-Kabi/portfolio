1395d7c400044aaf3ea03852072bfc33ecb1d3e4
Kavindu <kavinduperez@gmail.com>
2023-10-01 14:39:24 +0530

    intro section change with image

--- src/components/Intro/intro.js
+++ src/components/Intro/intro.js
@@ -9,9 +9,9 @@ const Intro = () => {
     <section id='intro'>
         <div className='introContent'>
             <span className='hello'>Hello,</span>
-            <span className='introText'>I'm<span className='intro Name'>Kavindu</span><br />Website Designer</span>
+            <span className='introText'>I'm<span className='introName'> Kavindu</span><br />Website Designer</span>
             <p className='introPara'>I am a skilled and passionate web designer with experience in creating visually appearing and user-friendly websites.</p>
-            <Link><button className='btn'><img src={btnImg} alt='hire me'/>Hire me</button></Link>
+            <Link><button className='btn'><img src={btnImg} alt='hire me' className='btnImg'/>Hire me</button></Link>
         </div>
         <img src={bg} alt='profile' className='bg'/>
     </section>
